957dfb5c50bad886684fa0fdfb46208908e99216
officersiam <siamalmahmud426@gmail.com>
2021-09-07 21:52:51 +0600

    Beautify Update

--- index.php
+++ index.php
@@ -321,11 +321,8 @@
             </div>
         </div>
     </div>
-
     <div class="uk-slider-container-offset" uk-slider>
-
         <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
-
             <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-4@s uk-grid" uk-slideshow="autoplay:true">
                 <li>
                     <div class="product-card">
@@ -424,14 +421,10 @@
                     </div>
                 </li>
             </ul>
-
             <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
             <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
-
         </div>
-
         <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
-
     </div>
 </section>
 <!-- ========== Blog SECTION end  ========== -->
